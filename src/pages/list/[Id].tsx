ae4af685cacf1121a1c3aefa08030aa4ae9b0127
LeeJiHyeon <rachel2148072@gmail.com>
2023-06-23 17:56:06 +0900

    feat: 상세페이지  getServerSideProps로 데이터 연결

--- src/pages/list/[Id].tsx
+++ src/pages/list/[Id].tsx
@@ -1,8 +1,66 @@
-export default function List() {
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next";
+import Error from "next/error";
+import Image from "next/image";
+
+export default function List({
+  bestSellers,
+  errorCode,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (errorCode) {
+    return <Error statusCode={errorCode} />;
+  }
   return (
     <>
-      <h1>TEST 1 BOOKS</h1>
-      <div>대충 리스트</div>
+      <h1>{bestSellers.list_name}</h1>
+      {bestSellers.books.map(book => (
+        <div key={book.amazon_product_url}>
+          <Image
+            src={book.book_image}
+            width={300}
+            height={300}
+            alt={book.title}
+          />
+          <span>{book.title}</span>
+          <span>{book.author}</span>
+          <a href={book.amazon_product_url}>Buy now &rarr;</a>
+        </div>
+      ))}
     </>
   );
 }
+type BestSellerList = {
+  list_name: string;
+  list_name_encoded: string;
+  bestsellers_date: string;
+  published_date_description: string;
+  previous_published_date: string;
+  display_name: string;
+  books: Book[];
+};
+type Book = {
+  rank: number;
+  publisher: string;
+  description: string;
+  title: string;
+  author: string;
+  contributor: string;
+  book_image: string;
+  amazon_product_url: string;
+};
+export const getServerSideProps: GetServerSideProps<{
+  bestSellers: BestSellerList;
+  errorCode?: number;
+}> = async ({ params }: GetServerSidePropsContext) => {
+  const res = await fetch(
+    `https://books-api.nomadcoders.workers.dev/list?name=${params?.id}`
+  );
+  const json = await res.json();
+  if (!res.ok || json.status === "ERROR") {
+    return { props: { errorCode: res.status, bestSellers: json } };
+  }
+  return { props: { bestSellers: json.results } };
+};
